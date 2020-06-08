6ce183ab9b5364bffc1d14a4e3edf7f2d0a4668c
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-06-07 21:50:15 -0700

    Set desired count to 2 in production

--- module/service.tf
+++ module/service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "app" {
   task_definition = aws_ecs_task_definition.app.arn
   cluster         = data.aws_ecs_cluster.main.id
 
-  desired_count = 1
+  desired_count = var.fp_context == "production" ? 2 : 1
   launch_type   = "FARGATE"
   network_configuration {
     subnets          = data.aws_subnet_ids.public.ids
@@ -14,5 +14,8 @@ resource "aws_ecs_service" "app" {
     target_group_arn = aws_alb_target_group.app.arn
     container_name   = "client"
     container_port   = var.client_port
+  }
+  lifecycle {
+    ignore_changes = [desired_count]
   }
 }
